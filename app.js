983bce273dbd0451be51af79ad1f30ad0b9c77d6
Alexpham688 <Alexpham688@gmail.com>
2017-08-10 09:11:35 -0400

    commit

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
 $(document).ready(function(){
-  $('.lowNav').on('click', function() {
-    $(this).find('.dropDown li').slideToggle(400);
-});
-  $('li.question').on('click', function(e) {
-    e.preventDefault();
+
+  $('li.question').on('click', function() {
+
     $(this).next()
       .slideToggle(500)
-        .siblings('.answers').slideUp(200);
-  });
+        .siblings('li.answer').slideUp();
+          var img = $(this).children('img');
+            $('img').not(img).removeClass('rotate');
+              img.toggleClass('rotate');
+            });
+
+
+
+  $('.lowNav').on('click', function() {
+    $(this).find('.dropDown li').slideToggle(400);
+    });
+
+    $('.navMenu ul li').hover(function() {
+      $(this).find("ul").stop().fadeToggle(400);
+    });
+    $("#navMenu ul li").click(function() {
+      $(this).find('ul').stop().fadeToggle(400);
+    });
 
 });
